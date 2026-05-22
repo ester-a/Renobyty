24541fda076006c0be3ee0e2266b449efaf664d6
ester-a <esterandrews2@gmail.com>
2026-05-22 17:28:11 +0200

    Clean up technical SEO

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig({
         services: resolve(__dirname, "services.html"),
         portfolio: resolve(__dirname, "portfolio.html"),
         portfolioDetail: resolve(__dirname, "portfolio-detail.html"),
-        contact: resolve(__dirname, "contact.html"),
       },
     },
   },
